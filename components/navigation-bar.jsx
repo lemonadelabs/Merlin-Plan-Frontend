04ff0e3a417ffe69e187b3a3dc6d7d787d91c29f
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-14 10:35:18 +1200

    added logout button

--- components/navigation-bar.jsx
+++ components/navigation-bar.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 import MenuButton from './menu-button'
+import { logout } from '../utilities/auth'
+import { withRouter } from 'react-router'
+
 function NavigationBar(props){
-  return(<header><MenuButton menuItems={props.menuItems}/></header>)
+  return(
+    <header>
+      <MenuButton menuItems={props.menuItems}/>
+      <a onClick={ () => handleLogout(props.router) }>Logout</a>
+    </header>
+  )
+}
+function handleLogout(router){
+  logout()
+  router.replace('login')
 }
 
-export default NavigationBar
+export default withRouter(NavigationBar)
