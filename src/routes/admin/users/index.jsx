d3b030f561ad07f701e91d13aa1f664b7212ddb5
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-23 13:53:46 +1200

    added fetching and displaying of users from the api

--- src/routes/admin/users/index.jsx
+++ src/routes/admin/users/index.jsx
@@ -4,6 +4,7 @@ import UserFixture from 'fixtures/users'
 import UserCard from 'components/user-card'
 import styles from './index.css'
 import { connect } from 'react-redux'
+import { getData } from 'utilities/api-interaction'
 
 class AdminUsers extends React.Component {
   constructor(){
@@ -14,11 +15,23 @@ class AdminUsers extends React.Component {
     console.log(UserFixture);
     console.log(this);
   }
+  componentDidUpdate(prevProps){
+    let {organisationId, dispatch} = this.props
+    if(prevProps.organisationId !== organisationId){//Feels a little hacky
+      this.getAndDespatchUsersData(organisationId,dispatch)
+    }
+  }
+  getAndDespatchUsersData(organisationId,dispatch){
+    getData(`organisation/${organisationId}/user`)
+    .then((orgUsers)=>{
+      dispatch({type:'SET_ORG_USERS', users:orgUsers})
+    })
+  }
   render() {
     return (
       <div className={styles['card-container']}>
         {
-          this.state.users.map(
+          this.props.users.map(
             (user) => {
               return (
                 <UserCard user={user}/>
@@ -36,6 +49,7 @@ const mapStateToProps = (state, ownProps) => {
   console.log(state); // state
   console.log(ownProps); // ownProps
   return(state)
+  return({users:state.orgUsers,organisationId:state.user.organisationId})
 }
 
 export default connect(mapStateToProps)(AdminUsers);
