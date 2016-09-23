a8d9f4b91fc37a79b266c6e4c4b2a52c6c78e0bf
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-23 13:52:28 +1200

    added SET_ORG_USERS to reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,15 @@
 const initialState = {
-  user:{}
+  user:{},
+  orgUsers:[]
 }
 function merlinApp(state = initialState, action){
   switch(action.type){
     case 'SET_USER' :
       return Object.assign({}, state, {user: action.userData})
+      break
+    case 'SET_ORG_USERS' :
+      console.log(action);
+      return Object.assign({}, state, {orgUsers: action.users})
       break
     default:
       return state
