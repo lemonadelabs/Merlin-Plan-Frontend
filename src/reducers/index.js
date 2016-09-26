a4215928f950052945aa67fb9c72ffc170fad999
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-27 09:02:05 +1300

    removed unreachable breaks

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -7,14 +7,11 @@ function merlinApp(state = initialState, action){
   switch(action.type){
     case 'SET_USER' :
       return Object.assign({}, state, {user: action.userData})
-      break
     case 'SET_ORG_USERS' :
       return Object.assign({}, state, {orgUsers: action.users})
-      break
     case 'SELECT_ORG_USER' :
       let selectedOrgUsers = [...state.selectedOrgUsers,action.user]
       return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsers})
-      break
     default:
       return state
   }
