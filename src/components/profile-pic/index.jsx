bb175e623f94c981e9a1ae92a2e5137ba0d868f5
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-27 09:56:25 +1300

    refactored profile pic to be a stateless component

--- src/components/profile-pic/index.jsx
+++ src/components/profile-pic/index.jsx
@@ -4,21 +4,16 @@ import hashbow from 'hashbow'
 import Color from 'color'
 import styles from './index.css'
 
-class ProfilePic extends React.Component {
+const ProfilePic = ({profilePic, firstName, lastName, id}) => {
+  let initials = firstName[0] + lastName[0]
+  let userColor = hashbow(firstName+lastName+id,60)
+  let userColorLight = Color(userColor).light()
 
-  render() {
-    let {profilePic, firstName, lastName, id}= this.props
-    let initials = firstName[0] + lastName[0]
-    let userColor = hashbow(firstName+lastName+id,60)
-    let userColorLight = Color(userColor).light()
-
-    return (
-      <div className={styles.container} style={{backgroundImage:`url(${profilePic})`,backgroundColor:userColor,color:userColorLight?'#333':'white'}}>
-        {profilePic?'':<p>{initials}</p>}
-      </div>
-    );
-  }
-
+  return (
+    <div className={styles.container} style={{backgroundImage:`url(${profilePic})`,backgroundColor:userColor,color:userColorLight?'#333':'white'}}>
+      {profilePic?'':<p>{initials}</p>}
+    </div>
+  );
 }
 
 export default ProfilePic;
