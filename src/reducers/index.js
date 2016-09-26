a412a550b3f413b13dc8f919aa1c23c8bc559a45
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-27 09:40:16 +1300

    added deselect reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,5 @@
+import findIndex from 'lodash/findIndex';
+
 const initialState = {
   user:{},
   orgUsers:[],
@@ -10,8 +12,14 @@ function merlinApp(state = initialState, action){
     case 'SET_ORG_USERS' :
       return Object.assign({}, state, {orgUsers: action.users})
     case 'SELECT_ORG_USER' :
-      let selectedOrgUsers = [...state.selectedOrgUsers,action.user]
-      return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsers})
+      let selectedOrgUsersWithNewSelection = [...state.selectedOrgUsers,action.user]
+      return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsersWithNewSelection})
+    case 'DESELECT_ORG_USER' :
+      let userIndex = findIndex(state.selectedOrgUsers, (user) => (user.id === action.user.id))
+      let selectedOrgUsersWithoutDeselectUser = [...state.selectedOrgUsers.slice(0,userIndex),
+                                                 ...state.selectedOrgUsers.slice(userIndex+1)]
+
+      return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsersWithoutDeselectUser})
     default:
       return state
   }
