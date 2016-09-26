ace936cce5bca16fbc460101e58805c8286e3576
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-26 14:28:38 +1300

    fixed race condition what would occur if the user data and the orgId were not defined on page load

--- src/routes/admin/users/index.jsx
+++ src/routes/admin/users/index.jsx
@@ -3,37 +3,44 @@ import React from 'react';
 import UserFixture from 'fixtures/users'
 import UserCard from 'components/user-card'
 import styles from './index.css'
-import { connect } from 'react-redux'
+import { connect, compose } from 'react-redux'
 import { getData } from 'utilities/api-interaction'
+import store from 'store'
 
 class AdminUsers extends React.Component {
   constructor(){
     super()
   }
-  componentDidUpdate(prevProps){
-    let {organisationId, dispatch} = this.props
-    if(prevProps.organisationId !== organisationId){//Feels a little hacky
-      this.getAndDespatchUsersData(organisationId,dispatch)
+  componentDidMount(){
+    console.log(store.getState());
+    let organisationId = store.getState().user.organisationId
+    if(organisationId){
+      this.getAndDespatchUsersData(organisationId)
+    }else{
+      this.waitForUserData()
     }
   }
-  getAndDespatchUsersData(organisationId,dispatch){
+  waitForUserData(){
+    let unsubscribeFromStore = store.subscribe(
+      () => {
+        let organisationId = store.getState().user.organisationId
+        if(organisationId){
+          this.getAndDespatchUsersData(organisationId)
+          unsubscribeFromStore()
+        }
+      }
+    )
+  }
+  getAndDespatchUsersData(organisationId){
     getData(`organisation/${organisationId}/user`)
     .then( (orgUsers) => {
-      dispatch({type:'SET_ORG_USERS', users:orgUsers})
+      store.dispatch({type:'SET_ORG_USERS', users:orgUsers})
     })
   }
   render() {
     return (
       <div className={styles['card-container']}>
-        {
-          this.props.users.map(
-            (user) => {
-              return (
-                <UserCard user={user}/>
-              )
-            }
-          )
-        }
+        { this.props.users.map( (user) => (<UserCard user={user}/>) ) }
       </div>
     );
   }
