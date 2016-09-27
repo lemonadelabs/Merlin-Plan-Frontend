e350e3bcfeb1df3e0aba1c304c6b42aee8d5eb59
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-09-27 16:05:58 +1300

    delete and add user reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,4 @@
-import findIndex from 'lodash/findIndex';
+import {findIndex,forEach, cloneDeep} from 'lodash';
 
 const initialState = {
   user:{},
@@ -11,6 +11,8 @@ function merlinApp(state = initialState, action){
       return Object.assign({}, state, {user: action.userData})
     case 'SET_ORG_USERS' :
       return Object.assign({}, state, {orgUsers: action.users})
+    case 'NEW_ORG_USER' :
+      return Object.assign({}, state, {orgUsers: [...state.orgUsers, action.user]})
     case 'SELECT_ORG_USER' :
       let selectedOrgUsersWithNewSelection = [...state.selectedOrgUsers,action.user]
       return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsersWithNewSelection})
@@ -20,6 +22,19 @@ function merlinApp(state = initialState, action){
                                                  ...state.selectedOrgUsers.slice(userIndex+1)]
 
       return Object.assign({}, state, {selectedOrgUsers: selectedOrgUsersWithoutDeselectUser})
+    case 'DELETE_ORG_USERS' :
+      let orgUsers = cloneDeep(state.orgUsers)
+      let orgUsersWithoutDeletedUsers = orgUsers
+      forEach(action.userIds,
+        (id) => {
+          let userIndex = findIndex(orgUsersWithoutDeletedUsers, (orgUser) => (orgUser.id === id))
+          orgUsersWithoutDeletedUsers = [...orgUsersWithoutDeletedUsers.slice(0,userIndex),
+                                         ...orgUsersWithoutDeletedUsers.slice(userIndex+1)]
+        }
+      )
+      return Object.assign({}, state, {orgUsers: orgUsersWithoutDeletedUsers})
+    case 'DESELECT_ALL_ORG_USERS' :
+      return Object.assign({}, state, {selectedOrgUsers:[]})
     default:
       return state
   }
