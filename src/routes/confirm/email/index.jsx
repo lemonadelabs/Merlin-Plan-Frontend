d7985ca01171f59c529019107a5d3ca721e18f7a
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-05 14:56:00 +1300

    makes the right api call now

--- src/routes/confirm/email/index.jsx
+++ src/routes/confirm/email/index.jsx
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
-import {getData} from 'utilities/api-interaction'
+import {postData} from 'utilities/api-interaction'
 
 class ConfirmEmail extends Component {
   confirm(email, code){
-    getData("frontendconfirmurl",{email:email,code:code})
+    postData("user/confirm",{email:email,code:code})
   }
   render() {
     console.log(this.props.params);
