efd4a981b8638feb731550c0069c9ebbfabe72bc
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-07 17:14:43 +1300

    Made it so that when you click next it logs into the app with your new password and moved to the confirm details route

--- src/routes/confirm/set-password/index.jsx
+++ src/routes/confirm/set-password/index.jsx
@@ -1,17 +1,31 @@
 import React, {Component} from 'react';
 import PasswordForm from 'components/forms/password'
 import { postData } from 'utilities/api-interaction';
-
+import { login } from 'utilities/auth'
+import { withRouter } from 'react-router'
 class ConfimSetPassword extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      emailCode:''
+      emailCode:'',
+      password:'',
+      passwordSet:false
     }
     this.handleChangeSuccess=this.handleChangeSuccess.bind(this)
+    this.handleClick = this.handleClick.bind(this)
   }
-  handleChangeSuccess(){
+  handleClick(e){
+    let email = this.props.location.query.email
+    let password = this.state.password
+    login(email,password)
+    .then(()=>{
+      this.props.router.push('/confirm/details')
+    })
+  }
+  handleChangeSuccess(password){
     console.log('Password set')
+    
+    this.setState({passwordSet:true,password:password})
   }
   componentDidMount(){
     let email = this.props.location.query.email
@@ -23,12 +37,18 @@ class ConfimSetPassword extends Component {
   }
   render() {
     let email = this.props.location.query.email
+    let passwordSet = this.state.passwordSet
+    let nextButton
+    if(passwordSet){
+      nextButton = <button onClick={this.handleClick}>Next</button>
+    }
     return (
       <div>
         <PasswordForm email={email} code={this.state.emailCode} handleChangeSuccess={this.handleChangeSuccess}/>
+        {nextButton}
       </div>
     );
   }
 }
 
-export default ConfimSetPassword;
+export default withRouter(ConfimSetPassword);
