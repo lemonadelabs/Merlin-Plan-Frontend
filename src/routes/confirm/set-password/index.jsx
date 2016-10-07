ef49592c748caec60b0db4d5e476ba32fec6987e
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-07 15:24:20 +1300

    set password route now will reset the password

--- src/routes/confirm/set-password/index.jsx
+++ src/routes/confirm/set-password/index.jsx
@@ -1,10 +1,32 @@
 import React, {Component} from 'react';
 import PasswordForm from 'components/forms/password'
+import { postData } from 'utilities/api-interaction';
+
 class ConfimSetPassword extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      emailCode:''
+    }
+    this.handleChangeSuccess=this.handleChangeSuccess.bind(this)
+  }
+  handleChangeSuccess(){
+    console.log('Password set')
+  }
+  componentDidMount(){
+    let email = this.props.location.query.email
+    postData('user/passwordreset',{email:email,sendEmail:false})
+      .then((result)=>{
+        let decodedCode = decodeURIComponent(result.code)
+        this.setState({emailCode:decodedCode})
+      })
+  }
   render() {
+    let email = this.props.location.query.email
+    console.log(this.state);
     return (
       <div>
-        <PasswordForm/>
+        <PasswordForm email={email} code={this.state.emailCode} handleChangeSuccess={this.handleChangeSuccess}/>
       </div>
     );
   }
