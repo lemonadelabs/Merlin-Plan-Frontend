9f54f3fd4544c8ac9e32a021a605c55e2cba7a73
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-07 15:23:26 +1300

    after confirm email go to set password

--- src/routes/confirm/email/index.jsx
+++ src/routes/confirm/email/index.jsx
@@ -7,12 +7,14 @@ class ConfirmEmail extends Component {
     super(props);
     this.confirm = this.confirm.bind(this)
   }
-  
   confirm(email, code){
     console.log(this.props);
     let emailConfimPromise = postData("user/confirm",{email:email, code:code})
     .then((result)=>{
-      this.props.router.push('/confirm/setPassword')
+      this.props.router.push({
+        pathname: '/confirm/password',
+        query: { email:email, code:code}
+      })
     })
     .catch((e)=>{
       console.error(e);
