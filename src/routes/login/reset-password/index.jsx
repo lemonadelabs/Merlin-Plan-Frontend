3c8a979e07db5032004f90b408e1ca295cdef1e5
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-11 14:42:15 +1300

    updated the login/reset-password route to handle the postData

--- src/routes/login/reset-password/index.jsx
+++ src/routes/login/reset-password/index.jsx
@@ -1,19 +1,24 @@
 import React, {Component} from 'react';
 import PasswordForm from 'components/forms/password'
 import { Link } from 'react-router';
+import { postData } from 'utilities/api-interaction';
+
 class ResetPassword extends Component {
   constructor(params) {
     super(params)
     this.state = {
       changeSucceded:false
     }
-    this.handleChangeSuccess = this.handleChangeSuccess.bind(this)
+    this.handlePasswordSet = this.handlePasswordSet.bind(this)
   }
-  handleChangeSuccess(){
-    this.setState({changeSucceded:true})
+  handlePasswordSet(password){
+    let {email, code} = this.props.location.query;    
+    postData('user/password',{email,code,password})
+    .then(()=>{
+      this.setState({changeSucceded:true})
+    })
   }
   render() {
-    let {email, code} = this.props.location.query;
     let changeSuccededMessage
     if(this.state.changeSucceded){
       changeSuccededMessage = <p>Password Change succeded, <Link to={'/login'}> return to login</Link></p>
@@ -21,7 +26,7 @@ class ResetPassword extends Component {
     return (
       <div>
         {changeSuccededMessage}
-        <PasswordForm email={email} code={code} handleChangeSuccess={this.handleChangeSuccess}/>
+        <PasswordForm handlePasswordSet={this.handlePasswordSet}/>
       </div>
     );
   }
