38bd354fb16c8ad0ff469af8b43d5cae0f8381a0
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-13 15:53:36 +1300

    fleshed out Groups Admin Route

--- src/routes/admin/groups/index.jsx
+++ src/routes/admin/groups/index.jsx
@@ -1,13 +1,40 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { getData } from 'utilities/api-interaction';
+import Modal from 'components/modal'
+import GroupForm from 'components/forms/group';
+
 class AdminGroups extends Component {
+  componentDidMount(){
+    let organisationId = this.props.organisationId || 1 //this needs to be fixed later!!!
+    let { dispatch } = this.props
+    getData(`organisation/${organisationId}/group`)
+      .then( groups => {
+        dispatch({type:'SET_ORG_GROUPS', groups})
+      })
+  }
   render() {
+    let groups = this.props.groups || []
     return (
       <div>
         <p>Admin Groups</p>
+        <GroupForm organisationId={1}/>
+        {
+          groups.map( group => {
+            return (
+              <div>
+                <p>{group.name}</p>
+                <p>{group.description}</p>
+              </div>)
+          })
+        }
       </div>
     );
   }
 }
 
-export default connect()(AdminGroups);
+function mapStateToProps(state, props) {
+  console.log(state);
+  return({organisationId:state.user.organisationId,groups:state.organisation.groups})
+}
+export default connect(mapStateToProps)(AdminGroups);
