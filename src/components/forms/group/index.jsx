c3832799c6023534873a7d71592afbeaba42bd3d
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-14 11:41:46 +1300

    added functionallity to add users to new groups

--- src/components/forms/group/index.jsx
+++ src/components/forms/group/index.jsx
@@ -1,27 +1,39 @@
 import React, {Component, PropTypes} from 'react';
 import { Form, Control, Errors, actions } from 'react-redux-form'
-import { postData } from 'utilities/api-interaction'
+import { getData, postData } from 'utilities/api-interaction'
 import { required } from 'components/forms/validators'
 import { connect } from 'react-redux';
+
 class GroupForm extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-  handleSubmit(groupInfo){
-    let { organisationId } = this.props
-    console.log(this.props);
-    let newGroupPayload = {}
-    Object.assign(newGroupPayload,groupInfo)
-    newGroupPayload.organisationId = organisationId
-    console.log(newGroupPayload);
-    postData('group',newGroupPayload)
-      .then( newGroup => { 
-        this.props.dispatch({type:'SET_ORG_GROUPS',groups:[newGroup]})
-        this.props.dispatch({type:"HIDE_MODAL"})
+  componentDidMount(){
+    let { organisationId, dispatch } = this.props
+    getData(`organisation/${organisationId}/user`)
+      .then( users => {
+        dispatch({type:'SET_ORG_USERS', users})
       })
   }
+  handleSubmit(groupInfo){
+    let { organisationId, dispatch } = this.props
+    let newGroupPayload = {}
+    Object.assign(newGroupPayload, {name: groupInfo.name, description: groupInfo.description})
+    newGroupPayload.organisationId = organisationId
+    console.log('groupInfo',groupInfo,'newGroupPayload',newGroupPayload);
+    postData('group',newGroupPayload)
+      .then( newGroup => {
+        this.addUsersToGroup(groupInfo.members, newGroup.id)
+        dispatch({type:'SET_ORG_GROUPS',groups:[newGroup]})
+        dispatch({type:"HIDE_MODAL"})
+      })
+  }
+  addUsersToGroup(users,groupId){
+    postData(`group/${groupId}/user`, {users})
+  }
   render() {
+    let users = this.props.users || []
     return (
       <Form model={'forms.group'} onSubmit={this.handleSubmit}>
         <label>Name</label>
@@ -29,7 +41,12 @@ class GroupForm extends Component {
         <Control.text model={'.name'} validators={{required}}/>
         <label>Description</label>
         <Errors model={'.description'} show="touched" messages={{required:"Please enter a description for your group"}}/>
-        <Control.text model={'.description'} validators={{required}}/>   
+        <Control.text model={'.description'} validators={{required}}/>
+        <Control.select model={'.members'} multiple={true}>
+          {
+            users.map( user => (<option key={user.id} value={user.id}>{`${user.firstName} ${user.lastName}`}</option>))
+          }
+        </Control.select>
         <button type="submit">Add Group</button>
       </Form>
     );
@@ -40,4 +57,11 @@ GroupForm.propTypes = {
 
 };
 
-export default connect()(GroupForm);
+function stateToProps(state, props) {
+  console.log(state);
+    return({
+      users : state.organisation.users
+    })
+}
+
+export default connect(stateToProps)(GroupForm);
