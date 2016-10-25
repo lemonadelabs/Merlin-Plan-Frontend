cd62243fc27f61a03c0213bc1ab5d3e137ccecb2
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-25 17:23:49 +1300

    update user form can now change user groups

--- src/components/forms/update-user/index.jsx
+++ src/components/forms/update-user/index.jsx
@@ -2,37 +2,77 @@ import React, {Component} from 'react';
 import { Control, Form, Errors, actions } from 'react-redux-form';
 import { connect } from 'react-redux';
 import UserDetails from 'components/forms/fields/user-details'
-import { putData } from 'utilities/api-interaction';
+import UserGroupField from 'components/forms/fields/user-group'
+import { forEach, findIndex, concat } from 'lodash';
+import { addUserToGroups, removeUserFromGroups } from 'utilities/user';
+import { getData, putData } from 'utilities/api-interaction';
 
 class UpdateUserForm extends Component {
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this)
   }
-  
   componentDidMount(){
-    let {modelToLoad, dispatch} = this.props
+    let {organisationId, modelToLoad, dispatch} = this.props
+    getData(`organisation/${organisationId}/group`)
+      .then((groups)=>{
+        let newGroupAction = {type:'SET_ORG_GROUPS', groups}
+        dispatch(newGroupAction)
+      })
     if(modelToLoad){
       dispatch(actions.merge('forms.user', modelToLoad))
     }
   }
   handleSubmit(user){
-    let {dispatch, handleDataUpdate} = this.props
+    let {dispatch, handleDataUpdate, modelToLoad} = this.props
     console.log('user',user);
+    let {groupsToAdd, groupsToRemove} = this.findGroupsToAddAndRemove(user.groups, modelToLoad.groups)
+    let groupAdditionRemovingPromises = concat(addUserToGroups(groupsToAdd, user.id), removeUserFromGroups(groupsToRemove, user.id))
+
     putData(`user/${user.id}`,user)
     .then((resp)=>{
-      handleDataUpdate(user)
-      dispatch({type:'HIDE_MODAL'})
+      Promise.all(groupAdditionRemovingPromises)
+        .then(
+          ()=>{
+            handleDataUpdate(user)
+            dispatch({type:'HIDE_MODAL'})
+        })
     })
   }
+  findGroupsToAddAndRemove(newGroups,oldGroups){
+    let groupsToAdd = []
+    let groupsToRemove = []
+    forEach(newGroups, newGroup => {
+      if(findIndex(oldGroups, oldGroup => ( newGroup.id === oldGroup.id )) === -1){
+        groupsToAdd.push(newGroup)
+      }
+    })
+    forEach(oldGroups, oldGroup => {
+      if(findIndex(newGroups, newGroup => ( newGroup.id === oldGroup.id )) === -1){
+        groupsToRemove.push(oldGroup)
+      }
+    })
+    return {groupsToAdd, groupsToRemove}
+  }
   render() {
+    let {organisationGroups} = this.props
     return (
       <Form model="forms.user" onSubmit={(userInfo)=>{this.handleSubmit(userInfo)}}>
         <UserDetails/>
+        <UserGroupField organisationGroups={organisationGroups}/>
         <button type="submit">Update Details</button>
       </Form>
     );
   }
 }
 
-export default connect()(UpdateUserForm);
+function propsToState(state, props) {
+  return(
+    {
+      organisationId:state.user.organisationId,
+      organisationGroups:state.organisation.groups
+    }
+  )
+}
+
+export default connect(propsToState)(UpdateUserForm);
