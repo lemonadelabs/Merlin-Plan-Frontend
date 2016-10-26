f6af2d8346de086143733ecb278844974b94dde5
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-26 16:58:47 +1300

    added colours base on name, subtle seperator line so that you can tell the difference between similar looking colors, very rough first pass of a key

--- src/components/partition-visualisation/index.jsx
+++ src/components/partition-visualisation/index.jsx
@@ -1,25 +1,45 @@
 import React, {Component} from 'react';
-import { Stage, Layer, Rect} from 'react-konva';
+import { Stage, Layer, Rect, Group} from 'react-konva';
 import { clone } from 'lodash';
+import hashbow from 'hashbow';
+
 class PartitionVisualisation extends Component {
   render() {
     let {width, height, available, partitions} = this.props
     let totalPixelsUsed = 0
     return (
-      <Stage width={width} height={height}>
-        <Layer>
+      <div>
+        <Stage width={width} height={height}>
+          <Layer>
+            {
+              partitions.map(
+                (partition) => {
+                  let x = clone(totalPixelsUsed)
+                  let partitionWidth = width * (partition.value / available)
+                  totalPixelsUsed += partitionWidth; 
+                  return (
+                    <Group x={x}>
+                      <Rect x={0} height={height} width={partitionWidth} fill={hashbow(partition.name)}/> 
+                      <Rect x={partitionWidth-1} height={height} width={1} fill={'rgba(0,0,0,0.15)'}/>
+                    </Group>
+                    ) }
+              )
+            }
+            {
+              renderUnusedPartition(totalPixelsUsed, width, height)
+            }
+          </Layer>
+        </Stage>
+        <div>
           {
             partitions.map(
-              (partition) => {
-                let x = clone(totalPixelsUsed)
-                let partitionWidth = width * (partition.value / available)
-                totalPixelsUsed += partitionWidth; 
-                return ( <Rect x={x} height={height} width={partitionWidth} fill={Konva.Util.getRandomColor()}/> )}
+              (partition) => (<p>`{partition.name}`</p>)
             )
           }
-          {renderUnusedPartition(totalPixelsUsed, width, height)}
-        </Layer>
-      </Stage>
+        </div>
+      </div>
+      
+      
     );
   }
 }
