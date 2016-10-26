18db0451cc3ccb922af26c434048ab33c07ebd21
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-10-26 16:57:05 +1300

    added ability to test-add partitions

--- src/routes/budget/index.jsx
+++ src/routes/budget/index.jsx
@@ -3,13 +3,23 @@ import PartitionVisualisation from 'components/partition-visualisation';
 class Budget extends React.Component {
   constructor(...args){
     super(...args)
+    this.state = {
+      partitions : [{name:'Baby Partition', value:100},{name:'Mummy Partition', value:1500},{name:'Daddy Partition', value:2500}]
+    }
+    this.addPartition = this.addPartition.bind(this)
+  }
+  addPartition(){
+    let partitions = this.state.partitions
+    let random = Math.random()
+    this.setState({partitions:[...partitions,{name:`Random Partition - ${random}`,value:random*2000}]})
   }
   render(){
-    let partitions = [{value:100},{value:4320},{value:6040}]
+    let {partitions} = this.state
     return(
       <div>
         <h1>Budget</h1>
-        <PartitionVisualisation width={500} height={40} available={10000} partitions={partitions} />
+        <PartitionVisualisation width={1000} height={40} available={10000} partitions={partitions} />
+        <button onClick={this.addPartition}>addPartition</button>
       </div>
     )
   }
