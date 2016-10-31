8e0acecd4735264757a7641260b41a3c47f879b0
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-01 11:38:50 +1300

    added action bar to nav so we have a place to put the actions for each route

---
+++ src/components/action-bar/index.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import styles from './styles.css'
+class ActionBar extends Component {
+  render() {
+    return (
+      <div className={styles.actionBar}>
+        
+      </div>
+    );
+  }
+}
+
+export default ActionBar;
--- src/components/navigation-bar/index.jsx
+++ src/components/navigation-bar/index.jsx
@@ -1,28 +1,31 @@
 import React, {PropTypes} from 'react'
-import MenuButton from 'components/menu-button'
-import { logout } from 'utilities/auth'
 import { withRouter } from 'react-router'
-import styles from './index.css'
+import { logout } from 'utilities/auth'
+import MenuButton from 'components/menu-button'
 import ProfilePic from 'components/profile-pic'
+import ActionBar from 'components/action-bar'
+import styles from './index.css'
 
 function NavigationBar({menuItems, applicationTitle, router, user}){
   return(
-    <header className={styles.navigationBar}>
-      <MenuButton menuItems={menuItems}/>
-      <p>{applicationTitle}</p>
-      <div className={styles.userInfo}>
-        <ProfilePic
-          firstName={user.firstName}
-          lastName={user.lastName}
-          profilePic={user.profilePic}
-          id={user.id}/>
-      <div className={styles.nameAndLogoutContainer}>
-        <p className={styles.userFullName}>{`${user.firstName} ${user.lastName}`}</p>
-        <a href="#" className={styles.logoutLink} onClick={ e => { e.preventDefault(); handleLogout(router) } }>Logout</a>
-      </div>
-      </div>
-    </header>
-
+    <div>
+      <header className={styles.navigationBar}>
+        <MenuButton menuItems={menuItems}/>
+        <p>{applicationTitle}</p>
+        <div className={styles.userInfo}>
+          <ProfilePic
+            firstName={user.firstName}
+            lastName={user.lastName}
+            profilePic={user.profilePic}
+            id={user.id}/>
+        <div className={styles.nameAndLogoutContainer}>
+          <p className={styles.userFullName}>{`${user.firstName} ${user.lastName}`}</p>
+          <a href="#" className={styles.logoutLink} onClick={ e => { e.preventDefault(); handleLogout(router) } }>Logout</a>
+        </div>
+        </div>
+      </header>
+      <ActionBar/>
+    </div>
   )
 }
 NavigationBar.propTypes = {
