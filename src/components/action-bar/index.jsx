458e091c1c76f66d3d59a8164d9b60035c43ad64
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-02 14:26:38 +1300

    added action bar

--- src/components/action-bar/index.jsx
+++ src/components/action-bar/index.jsx
@@ -1,13 +1,34 @@
-import React, {Component} from 'react';
+import React, {Component, PropTypes} from 'react'
+import { connect } from 'react-redux'
+import { forEach } from 'lodash';
 import styles from './styles.css'
+
 class ActionBar extends Component {
+  dispatchActions(actionsToDispatch){
+    let dispatch = this.props.dispatch
+    forEach(actionsToDispatch, action => { dispatch(action) } )
+      
+  }
   render() {
+    let {actions, dispatch} = this.props
     return (
       <div className={styles.actionBar}>
-        
+        <div className={styles.actionContainer}>
+          {
+            actions.map( action => ( <p className={styles.action} onClick={ () => { this.dispatchActions(action.actionsToDispatch) } } > {action.title} </p> ) )
+          }
+        </div>
       </div>
     );
   }
 }
 
-export default ActionBar;
+ActionBar.propTypes = {
+  actions: PropTypes.array
+}
+
+function stateToProps(state){
+  return {actions:state.app.availableActions}
+}
+
+export default connect(stateToProps)(ActionBar);
