92c7be14aa8b650a48519d1ce9478f52be167464
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-02 17:06:36 +1300

    removed a mailto link from user details because it was annoying to accidentally open the email when tying to select a user

--- src/components/user-details/index.jsx
+++ src/components/user-details/index.jsx
@@ -7,7 +7,7 @@ class UserDetails extends Component {
     return (
       <div className={styles.container}>
         <h3>{`${user.firstName} ${user.lastName}`}</h3>
-        <a href={`mailto:${user.email}`}>{user.email}</a>
+        <p>{user.email}</p>
         <p>{`${user.id}`}</p>
       </div>
     );
