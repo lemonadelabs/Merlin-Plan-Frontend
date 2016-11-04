82f67b711782c8819c98371bb67ecdd77f140966
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-04 16:04:47 +1300

    resources route now gets the resources accessable to the user and displays them

--- src/routes/budget/index.jsx
+++ src/routes/budget/index.jsx
@@ -1,28 +1,69 @@
 import React from 'react'
-import PartitionVisualisation from 'components/partition-visualisation';
+import { connect } from 'react-redux';
+import { getData } from 'utilities/api-interaction';
+import { map, union } from 'lodash'
+// import PartitionVisualisation from 'components/partition-visualisation';
+
 class Budget extends React.Component {
   constructor(...args){
     super(...args)
     this.state = {
-      partitions : [{name:'Baby Partition', value:100},{name:'Mummy Partition', value:1500},{name:'Daddy Partition', value:2500}]
+      partitions : [{name:'Baby Partition', value:100},{name:'Mummy Partition', value:1500},{name:'Daddy Partition', value:2500}],
+      scenarios:[]
     }
     this.addPartition = this.addPartition.bind(this)
+  }
+  componentDidMount(){
+    let {userId,dispatch} = this.props
+    dispatch({ type:"SET_VISABILITY_FILTER", filter: "ALL" })
+    dispatch({ type:"SET_ACTIONS", actions:[{title:"New Scenario"}] })
+    getData(`resourcescenario/useraccess/${userId}`)
+    .then( scenarios => {
+      dispatch({ type:"SET_ACCESSABLE_SCENARIOS", scenarios })
+      // console.log( union( ...map(result, scenarios => (scenarios) ) ) );
+    })
   }
   addPartition(){
     let partitions = this.state.partitions
     let random = Math.random()
     this.setState({partitions:[...partitions,{name:`Random Partition - ${random}`,value:random*2000}]})
   }
+
   render(){
-    let {partitions} = this.state
+    let {scenarios} = this.props
+    let scenariosFiltered = filterScenarios(scenarios,"ALL")
     return(
       <div>
         <h1>Budget</h1>
-        <PartitionVisualisation width={1000} height={40} available={10000} partitions={partitions} />
-        <button onClick={this.addPartition}>addPartition</button>
+        {
+          scenariosFiltered.map(
+            scenario => {
+              return ( <p>{scenario.name}</p> )
+            }
+          )
+        }
       </div>
     )
   }
 }
 
-export default Budget
+function filterScenarios(allScenarios, filterState){
+  let scenariosToReturn
+  switch (filterState) {
+    case 'ALL': {
+      let scenariosFlattened = map(allScenarios, scenarios => (scenarios))
+      scenariosToReturn = union( ...scenariosFlattened )
+      break
+    }
+    default:
+      scenariosToReturn = []
+      break;
+  }
+  return scenariosToReturn
+}
+
+function stateToProps(state){
+  return { userId: state.user.id, scenarios:state.resources.accessableScenarios}
+}
+
+export default connect(stateToProps)(Budget)
