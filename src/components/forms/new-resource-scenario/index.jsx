39a17af868524df8a7f6a1cdbab04b476bd08b57
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-07 15:04:50 +1300

    added new resource scenario form component

---
+++ src/components/forms/new-resource-scenario/index.jsx
@@ -0,0 +1,49 @@
+import React, {Component} from 'react'
+import { connect } from 'react-redux'
+import { Form, Control } from 'react-redux-form'
+import {postData} from 'utilities/api-interaction';
+
+class NewResourceScenarioForm extends Component {
+  render() {
+    let {userGroups, userEmail} = this.props
+    let selectGroup = userGroups.length > 1
+
+    return (
+      <Form model={"forms.resourceScenario"} onSubmit={formData => { handleFormSubmit(formData,userGroups,userEmail) }}>
+        <label>Resource Scenario Name</label>
+        <Control.text model={".name"}/>
+        {selectGroup ? addGroupDropDown(userGroups) : ''}
+        <button type="submit">Add Scenario</button>
+      </Form>
+    );
+  }
+}
+
+function handleFormSubmit(formData,userGroups, userEmail){
+  let payload = Object.assign({},formData,{group:userGroups[0].id, creator:userEmail})
+  postData('resourcescenario',payload)
+  .then( newResourceScenario =>{
+    console.log(newResourceScenario);
+  } )
+}
+
+function addGroupDropDown(groups){
+  return(
+    <div>
+      <label>Which group  do you want this Scenario to be added to:</label>
+      <Control.select model={".group"}>
+        {
+          groups.map( group => (<option value={group.id} >{group.name}</option>))
+        }
+      </Control.select>
+    </div>
+  )
+}
+
+function stateToProps(state) {
+  return({
+    userEmail: state.user.email,
+    userGroups: state.user.groups
+  })
+}
+export default connect(stateToProps)(NewResourceScenarioForm);
