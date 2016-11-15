26c615ac95808e847f245b47be3c4bfe825532cd
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-16 12:26:36 +1300

    resources view now show financial resources

--- src/routes/resources/view/index.jsx
+++ src/routes/resources/view/index.jsx
@@ -1,18 +1,46 @@
 import React, {Component} from 'react';
 import { getData } from 'utilities/api-interaction'
+import {TimelineObject} from 'components/timeline-object'
+import Timeline from 'components/timeline'
 
 class ResourcesView extends Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      financialResources:[]
+    }
+  }
+  
   componentDidMount() {
     let {scenarioId} = this.props.params
     getData(`resourcescenario/${scenarioId}/resources`)
-    .then( resourcesData => { console.log(resourcesData) } )
+    .then( resourcesData => {this.setState({financialResources:resourcesData.financialResources})} )
+  }
+  timelineObjectsForFinancialResources(financialResources){
+    let timelineObjects = []
+    console.log(financialResources);
+    timelineObjects = financialResources.map( 
+      financialResource => (
+        <TimelineObject
+          name={financialResource.name}
+          startDate={financialResource.startDate}
+          endDate={financialResource.endDate}/>
+      )
+    )
+    timelineObjects.push(
+
+    )
+    return timelineObjects
   }
   render() {
-    return (
+    let financialResourceTimelineObjects = this.timelineObjectsForFinancialResources(this.state.financialResources)
+    return(
       <div>
-        
+        <Timeline timelineStartYear={2016} numberOfYears={10}>
+          {financialResourceTimelineObjects}
+        </Timeline>
       </div>
-    );
+    )
   }
 }
 
