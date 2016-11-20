e1001c983e3c17bbc2ff7d19ab205d977b1a9341
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-21 10:08:20 +1300

    Added reducer to update financial resources

--- src/reducers/resources/index.js
+++ src/reducers/resources/index.js
@@ -1,3 +1,5 @@
+import {findIndex, forEach, cloneDeep} from 'lodash';
+
 export default function resources(state = { accessableScenarios:[],scenarioData:{},chartData:{}}, action) {
   switch (action.type) {
     case 'SET_ACCESSABLE_SCENARIOS':
@@ -17,6 +19,15 @@ export default function resources(state = { accessableScenarios:[],scenarioData:
       }
       newDataChartObject.chartData[action.id] = action.data
       return Object.assign({},state,newDataChartObject)
+    }
+    case 'UPDATE_FINANCIAL_RESOURCE': {
+      let financialResources = cloneDeep(state.scenarioData[action.resourceScenarioId].financialResources)//not positive that this needs to be cloned, but safety first.
+      let resourceToUpdateIndex = findIndex( financialResources, financialResource => (financialResource.id === action.id) )
+      let updatedFinancialResources = [...financialResources.splice(0,resourceToUpdateIndex), action.updatedFinancialResource,...financialResources.splice(resourceToUpdateIndex+1)]
+      let updatedScenarioData = {}
+      updatedScenarioData[action.resourceScenarioId] = Object.assign({}, state.scenarioData[action.resourceScenarioId], {financialResources:updatedFinancialResources})
+      let updatedScenarioDataList = Object.assign({}, state.scenarioData, updatedScenarioData)
+      return Object.assign({},state,{scenarioData:updatedScenarioDataList})
     }
     default:
       return state;
