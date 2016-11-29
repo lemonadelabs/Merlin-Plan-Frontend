a94a9d23ff936f68cd56c5e89dd6bb8ddc67910e
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-11-29 15:54:17 +1300

    added display of selected date

--- src/components/forms/controls/date-picker/index.jsx
+++ src/components/forms/controls/date-picker/index.jsx
@@ -14,15 +14,19 @@ function DatePicker ({model, dispatch}) {
       }}
       mapProps={{
         selectedValue: props => (props.modelValue),
+        viewValue: props => (props.viewValue),
         model: props => (props.model)
       }}
     />
   );
 }
 
-function Picker ( {model, dispatch, selectedValue} ) {
+function Picker ( {model, dispatch, viewValue, selectedValue} ) {
   return (
     <div>
+      <div>
+        <p>{new Date(viewValue).toLocaleDateString()}</p>
+      </div>
       <DayPicker 
         onDayClick={ (e,day) => dispatch( actions.change(model, day) ) } 
         selectedDays= { day => DateUtils.isSameDay(day, new Date(selectedValue) ) }/>
