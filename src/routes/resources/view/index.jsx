24e4433ea34f71a2203a13557c9e9e9c2d0ec734
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-12-12 13:20:25 +1300

    added sharing stuff to the action bar for financial resource scenario stuff

--- src/routes/resources/view/index.jsx
+++ src/routes/resources/view/index.jsx
@@ -1,21 +1,68 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
-import { getResourceScenarioData } from 'actions/resource-scenario';
+import { find,isEqual } from 'lodash'
+import  * as actions from 'actions/resource-scenario';
 import FinancialScenarioView from 'components/financial-scenario-view';
 
 class ResourcesView extends Component {
   componentDidMount() {
     let {scenarioId} = this.props.params
-    let {dispatch} = this.props
-    dispatch( getResourceScenarioData(scenarioId) )
+    let {dispatch,scenarioMetadata} = this.props
+    let sharingSettings = scenarioMetadata ? scenarioMetadata.sharing : {groupShared:false,organisationShared:false,userShare:[]}
+    if(!scenarioMetadata){
+      dispatch( actions.getResourceScenarioMetadata(scenarioId) )
+    }
+    dispatch( actions.getResourceScenarioData(scenarioId) )
     dispatch({ 
       type:"SET_ACTIONS", 
       actions:[
-        {title:"Share Scenario To Group ", 'actionsToCreatorsToRun':[{name:'shareToGroup', props:{"endPoint":"resourcescenario","id":1}}]},
-        {title:"Share Scenario To Organisation", 'actionsToCreatorsToRun':[{name:'shareToOrganisation', props:{"endPoint":"resourcescenario","id":1}}]},
-        {title:"Share Scenario To User",  'actionsToCreatorsToRun':[{name:'shareToUsers', props:{"endPoint":"resourcescenario","id":1,"users":["caleb.sawtell@lemonadelabs.io"]}}]}
-        ]})
+        ...this.createSharingActions(sharingSettings)
+      ]
+    })
 
+  }
+  componentWillUpdate(nextProps, nextState) {
+    console.log(nextProps);
+    if(!isEqual(nextProps.scenarioMetadata,this.props.scenarioMetadata) ){
+      console.log('Updated metaData, ');
+      this.props.dispatch({ 
+      type:"SET_ACTIONS", 
+      actions:[
+        ...this.createSharingActions(nextProps.scenarioMetadata.sharing)
+      ]
+    })
+    }
+  }
+  createSharingActions(sharingSettings){
+    let sharingAction = [
+      {
+        title:sharingSettings.groupShared ? "Unshare Scenario With Group" : "Share Scenario With Group",
+        'actionsToCreatorsToRun':[
+          {
+            name: sharingSettings.groupShared ? 'unshareToGroup' : 'shareToGroup',
+            props: {
+              "endPoint":"resourcescenario",
+              "id":1
+            }
+          }
+        ]
+      },
+      {
+        title:sharingSettings.organisationShared ? "Unshare Scenario With Organisation" :"Share Scenario With Organisation",
+        'actionsToCreatorsToRun':[
+          {
+            name: sharingSettings.organisationShared ? 'unshareToOrganisation' : 'shareToOrganisation',
+            props:{
+              "endPoint":
+              "resourcescenario",
+              "id":1
+            }
+          }
+        ]
+      },
+      {title:"Share Scenario To User",  'actionsToCreatorsToRun':[{name:'shareToUsers', props:{"endPoint":"resourcescenario","id":1,"users":["caleb.sawtell@lemonadelabs.io"]}}]}
+    ]
+    return sharingAction
   }
   render() {
     const scenarioId = this.props.params.scenarioId
@@ -26,7 +73,11 @@ class ResourcesView extends Component {
     )
   }
 }
-// function mapStateToProps(state) {
-//   return()
-// }
-export default connect()(ResourcesView);
+function mapStateToProps(state, props) {
+  let scenarioMetadata = find(state.resources.scenarios, scenario => ( scenario.id === Number(props.params.scenarioId) ) )
+  return ({
+    scenarioMetadata,
+    ...props
+  })
+}
+export default connect(mapStateToProps)(ResourcesView);
