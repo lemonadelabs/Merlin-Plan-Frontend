da2b5c08a03c58b9452207e21c351f8c10e9675e
Caleb Sawtell <caleb.sawtell@gmail.com>
2016-12-16 10:45:08 +1300

    added arrow select and resetting of the field when you slect and entry

--- src/components/fuzzy-search-input/index.jsx
+++ src/components/fuzzy-search-input/index.jsx
@@ -6,8 +6,12 @@ class FuzzySearchInput extends Component {
   constructor(props, context) {
     super(props, context);
     this.handleChange = this.handleChange.bind(this)
+    this.handleKeyUp = this.handleKeyUp.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
     this.state = {
       results:[],
+      selectedEntry:0,
+      selectingEntry:false,      
       options: {
         threshold:0.4,
         distance:10,
@@ -24,19 +28,51 @@ class FuzzySearchInput extends Component {
   handleChange(e){
     let searchTerm = e.target.value
     let results = this.fuse.search(searchTerm)
-    this.setState({results})
+    this.setState({results,searchTerm})
+  }
+  handleKeyUp(e){
+    if(e.key === "ArrowDown" || e.key === "ArrowUp"){
+      let selectedEntry = this.state.selectedEntry
+      switch (e.key) {
+        case "ArrowDown":
+          if(selectedEntry < this.state.results.length - 1 && this.state.selectingEntry){
+            selectedEntry++
+          }
+          break;
+        case "ArrowUp":
+          if(selectedEntry>0 && this.state.selectingEntry){
+            selectedEntry--
+          }
+          else{
+            selectedEntry=this.state.results.length - 1
+          }
+          break;
+        default:
+          break;
+      }
+      this.setState({selectedEntry,selectingEntry:true})
+    }
+    else{
+      this.setState({selectedEntry:0,selectingEntry:false})
+    }
+  }
+  handleSubmit(e){
+    e.preventDefault()
+    let result = this.state.results[this.state.selectedEntry]
+    this.props.handleSelection(result)
+    this.setState({searchTerm:'',results:[]})
   }
   render() {
     return (
       <div>
-        <form onSubmit={ e => { e.preventDefault(); let result = this.state.results[0]; this.props.handleSelection(result)} }>
-          <input placeholder={this.props.placeholder} onChange={this.handleChange}/>
+        <form onSubmit={ this.handleSubmit }>
+          <input value={ this.state.searchTerm } placeholder={this.props.placeholder} onChange={this.handleChange} onKeyUp={this.handleKeyUp}/>
         </form>
         <div style={{border:'solid 1px white'}}>
           {
             this.state.results.map(
-              result => {
-                return (<p onClick={ e => {this.props.handleSelection(result);} }>{`${result.firstName} ${result.lastName}`}</p>)
+              (result, index) => {
+                return (<p style={{backgroundColor:this.state.selectedEntry===index&&this.state.selectingEntry?'blue':'transparent'}} onClick={ e => {this.props.handleSelection(result);    this.setState({searchTerm:'',results:[]});} }>{`${result.firstName} ${result.lastName}`}</p>)
               }
             )
           }
